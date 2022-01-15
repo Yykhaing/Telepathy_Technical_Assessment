e44a5c21f590b09996216bcb8bb4a5d367ae201c
Yu Yu Khaing <yukhaing@gmail.com>
2022-01-15 19:18:24 +0800

    add hotelroomservice class

---
+++ Telepathy_Technical_Assessment/Task_1/HotelRoomService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telepathy_Technical_Assessment.Task_1
+{
+    class HotelRoomService : IHotelRomService
+    {
+        int IHotelRomService.AssignRoom()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IHotelRomService.CheckOutRoom(int RoomId)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IHotelRomService.CleanRoom(int RoomId)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IHotelRomService.GetAllAvailableRooms()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IHotelRomService.RepairRoom(int RoomId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
